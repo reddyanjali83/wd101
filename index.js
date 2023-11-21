79be61e4d71f99b444ba7514cfe837dac2560536
reddyanjali83 <151562819+reddyanjali83@users.noreply.github.com>
2023-11-21 22:28:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,79 @@
-function minDate() {
-    const today = new Date();
-    return new Date(today.getFullYear() - 55, today.getMonth(), today.getDate()).toISOString().split('T')[0];
+let userForm=document.getElementById('user');
+let arr=[];
+const ret=()=>{
+    let entry=JSON.parse(localStorage.getItem('arr'))||[];
+    return entry;
 }
 
-function maxDate() {
-    const today = new Date();
-    return new Date(today.getFullYear() - 18, today.getMonth(), today.getDate()).toISOString().split('T')[0];
-}
-
-const dobInput = document.getElementById('dob');
-dobInput.setAttribute('min', minDate());
-dobInput.setAttribute('max', maxDate());
-
-let userForm = document.getElementById("userForm");
-
-const getEntries = () => {
-    let entries = localStorage.getItem("userEntries");
-    if (entries) {
-        entries = JSON.parse(entries);
-    } else {
-        entries = [];
-    }
-    return entries;
-}
-
-let userEntries = getEntries();
-
-const dispEntries = () => {
-    const entries = getEntries();
-    const tableEntries = entries.map((entry) => {
-        const name = `<td class="bor">${entry.name}</td>`;
-        const email = `<td class="bor">${entry.email}</td>`;
-        const password = `<td class="bor">${entry.password}</td>`;
-        const dateOfBirth = `<td class="bor">${entry.dob}</td>`; 
-        const atnc = `<td class="bor">${entry.atnc}</td>`;
-
-        const row = `<tr>${name} ${email} ${password} ${dateOfBirth} ${atnc}</tr>`;
+const dis=()=>{
+    let entry=ret();
+    const tE=entry.map((input)=>{
+        const namedata=`<td class='th'>${input.FullName}</td>`;
+        const emaildata=`<td class='th'>${input.email}</td>`;
+        const passworddata=`<td class='th'>${input.password}</td>`;
+        const dobdata=`<td class='th'>${input.dob}</td>`;
+        const termsdata=`<td class='th'>${input.terms}</td>`;
+        const row=`<tr>${namedata} ${emaildata} ${passworddata} ${dobdata} ${termsdata}</tr>`;
         return row;
-    }).join("\n");
-
-    const table = `<h1>Entries</h1><table class="table"><tr class="bor"><th class="bor">Name</th><th class="bor">Email</th><th class="bor">Password</th><th class="bor">Dob</th><th class="bor">Accepted terms?</th></tr>${tableEntries}</table>`;
-
-    let details = document.getElementById("tableView");
-    details.innerHTML = table;
+    }).join('\n');
+    const tabBody=document.querySelector('#user-table tbody');
+    tabBody.innerHTML=tE;
 }
+const abc = document.getElementById("dob");
+abc.addEventListener("change", () =>
+{
+    let k=abc.value.split("-");
+    let birthdate = new Date(k[0],k[1],k[2]);
+    let today = new Date();
+    let present_year= today.getFullYear();
+    let birth_Year=birthdate.getFullYear()
+    let age = present_year - birth_Year;
+    let month_Diff = today.getMonth() - birthdate.getMonth();
 
-dispEntries();
-
-const formSubmit = (event) => {
+    if ((today.getDate() < birthdate.getDate())||month_Diff<0) 
+        {
+        age--;
+        }
+    if (age<18 || age>55) 
+        {
+        abc.setCustomValidity("Age must be between 18 and 55");
+        abc.reportValidity();
+    }else
+        {
+        abc.setCustomValidity("");
+        }
+    }
+);
+const suf=(event)=>{
     event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const dateOfBirth = document.getElementById("dob").value; 
-    const atnc = document.getElementById("atnc").checked;
-
-    const entry = {
-        name, email, password, dob: dateOfBirth, atnc 
+    const FullName = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const dob = document.getElementById('dob').value
+    const terms = document.getElementById('terms').checked
+    // var currentYear = new Date().getFullYear();
+    // var birthYear = dob.split("-");
+    // let year=birthYear[0]
+    // var age = currentYear-year
+    // if(age < 18 || age > 55){
+    //     let k=document.getElementById('dob')
+        
+      
+    //   return alert("Age must be between 18 and 55")
+    // }
+    // else
+    // {
+    // document.getElementById('dob')
+    const input={
+        FullName,email,password,dob,terms
+    };
+    arr=ret();
+    arr.push(input);
+    localStorage.setItem("arr",JSON.stringify(arr));
+    dis();
+    userForm.reset();
     }
 
-    userEntries.push(entry);
-    localStorage.setItem("userEntries", JSON.stringify(userEntries)); 
-    dispEntries();
-}
 
-userForm.addEventListener("submit", formSubmit);
-
+userForm.addEventListener('submit',suf)
+dis();
